c4a873cdfc723081e3ecca080e9303b9860b8758
dbgalz11 <68313302+dbgalz11@users.noreply.github.com>
2021-10-01 21:21:17 +0800

    Update main.tf

--- Part-I-Tools/Azure Pipelines/VM/main.tf
+++ Part-I-Tools/Azure Pipelines/VM/main.tf
@@ -17,7 +17,9 @@ resource "azurerm_virtual_machine" "NoBSDevVM" {
   name                  = "nobssvm"
   location              = var.location
   resource_group_name   = var.resourceGroupName
-  network_interface_ids = ["${var.network_interface_id}"]
+  network_interface_ids = [
+    var.network_interface_id,
+  ]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
